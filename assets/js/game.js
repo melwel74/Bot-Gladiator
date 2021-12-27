25801393a8879b4b37862105e8556285aeb3debf
Melvin Welton <nivlem65@gmail.com>
2021-12-27 17:16:34 -0600

    commit to feature/initial-game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,23 @@
-var playerName = window.prompt("What's your robot name?");
-console.log(playerName)
-console.log("this logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is" + playerName);
+// var playerName = window.prompt("What's your robot name?");
+// console.log(playerName)
+// console.log("this logs a string, good for leaving yourself a message");
+// console.log(10 + 10);
+// console.log("Our robot's name is" + playerName);
 
-function fight() {
-    window.alert("the fight has begun");
-}
+// function fight() {
+//     window.alert("the fight has begun");
+// }
 // fight()
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerHealth, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Bot Gladiators!");
+};
+fight();
