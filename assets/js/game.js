29d7d5a63d3442234892c3e63481aa64dd3a0c88
Melvin Welton <nivlem65@gmail.com>
2021-12-27 17:03:49 -0600

    commit on feature/intial-game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,10 @@
-// window.alert("this is an alert! Javascript is running!")
+var playerName = window.prompt("What's your robot name?");
+console.log(playerName)
+console.log("this logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is" + playerName);
 
 function fight() {
     window.alert("the fight has begun");
 }
+// fight()
