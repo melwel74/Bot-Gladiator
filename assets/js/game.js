1ee62b0f652ab871a745366a991743d759eb28b4
Melvin Welton <nivlem65@gmail.com>
2021-12-27 19:31:43 -0600

    commit to feature/initial-game

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,3 +21,12 @@ var fight = function() {
     window.alert("Welcome to Bot Gladiators!");
 };
 fight();
+
+// Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
+enemyHealth = enemyHealth - playerAttack
+// Log a resulting message to the console so we know that it worked
+console.log(
+playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+);
+// Subtract the value of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
+// Log a resulting message to the console so we know that it worked
